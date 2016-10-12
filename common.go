c1414f68301a3481ea3332bde48ffec13d90d36b
Olivier Mengué <dolmen@cpan.org>
2016-10-12 23:54:29 +0200

    Add Stringer interface

--- common.go
+++ common.go
@@ -3,3 +3,8 @@ package benchmark
 import "errors"
 
 var errSetRoot = errors.New("This implementation doesn't handle replacing the document root")
+
+// same as fmt.Stringer
+type Stringer interface {
+	String() string
+}
