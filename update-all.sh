bdad392397dd90416e33e74c665a0d31eba34365
Olivier Mengué <dolmen@cpan.org>
2019-02-26 14:47:21 +0100

    update-all.sh: update dependencies in both Gopkg.lock and go.mod

--- update-all.sh
+++ update-all.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
+export GO111MODULE
+
+GO111MODULE=off
+rm -Rf vendor
 go list -f '{{join .Imports "\n"}}' | grep '^github.com' | xargs go get -u -v
 dep ensure -update
+
+GO111MODULE=on
+go list -f '{{join .Imports "\n"}}' | grep '^github.com' | while read pkg
+do
+	go mod edit -require $pkg@master
+	go build
+done
