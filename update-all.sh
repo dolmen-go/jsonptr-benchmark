98edce48a754d772ae80649d331de15e2f7478ce
Olivier Mengué <dolmen@cpan.org>
2019-09-24 15:27:45 +0200

    Abandon dep-based dependency management

--- update-all.sh
+++ update-all.sh
@@ -2,14 +2,10 @@
 
 export GO111MODULE
 
-GO111MODULE=off
-rm -Rf vendor
-go list -f '{{join .Imports "\n"}}' | grep '^github.com' | xargs go get -u -v
-dep ensure -update
-
 GO111MODULE=on
 go list -f '{{join .Imports "\n"}}' | grep '^github.com' | while read pkg
 do
-	go mod edit -require $pkg@master
+	go mod edit -require $pkg@latest
 	go build
+	go mod tidy
 done
